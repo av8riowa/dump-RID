58e55931b7863bce3fe0ae29c346fdc7d63b3f7c
av8riowa <160094983+av8riowa@users.noreply.github.com>
2024-02-19 22:54:30 -0600

    Update anet.h for RID. needs work

--- anet.h
+++ anet.h
@@ -1,4 +1,4 @@
-/* anet.c -- Basic TCP socket stuff made a bit less boring
+/* anet.h -- Network functions for dump-RID project
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -20,25 +20,7 @@
 #define ANET_ERR -1
 #define ANET_ERR_LEN 256
 
-#if defined(__sun)
-#define AF_LOCAL AF_UNIX
-#endif
-
-int anetTcpConnect(char *err, char *addr, int port);
-int anetTcpNonBlockConnect(char *err, char *addr, int port);
-int anetUnixConnect(char *err, char *path);
-int anetUnixNonBlockConnect(char *err, char *path);
-int anetRead(int fd, char *buf, int count);
-int anetResolve(char *err, char *host, char *ipbuf);
-int anetTcpServer(char *err, int port, char *bindaddr);
-int anetUnixServer(char *err, char *path, mode_t perm);
-int anetTcpAccept(char *err, int serversock, char *ip, int *port);
-int anetUnixAccept(char *err, int serversock);
-int anetWrite(int fd, char *buf, int count);
-int anetNonBlock(char *err, int fd);
-int anetTcpNoDelay(char *err, int fd);
-int anetTcpKeepAlive(char *err, int fd);
-int anetPeerToString(int fd, char *ip, int *port);
-int anetSetSendBuffer(char *err, int fd, int buffsize);
+int anetInitializeNetwork(); // Initialize network connections for dump-RID project
+int anetBroadcastMessage(const char *message); // Broadcast message over the network
 
 #endif
